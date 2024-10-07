b76e609344812bf7425a78225e84ebd7a228549d
Ayush Kumar <ak433778@gmail.com>
2024-10-08 00:20:57 +0530

    online server

--- db.js
+++ db.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 //define the MongoDB connections URL
-const mongoURL = "mongodb://localhost:27017/hotels";
+// const mongoURL = "mongodb://localhost:27017/hotels";
+const mongoURL = process.env.DB_URL;
 
 // Set up MongoDb connections
 mongoose.connect(mongoURL, {});
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const db = require("./db");
+require("dotenv").config();
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json()); //req.body
@@ -15,6 +16,8 @@ app.use("/person", personRoutes);
 const menuRoutes = require("./routes/menuRoutes");
 app.use("/menu", menuRoutes);
 
-app.listen(3000, () => {
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
   console.log("listening");
 });
